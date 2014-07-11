eef67429f32708e92337b400e0b7be493f26de2e
AlessandroPerina <alessandro.perina@gmail.com>
2014-07-11 21:45:53 +0200

    main_eigen.cpp che funziona. Ho solo giocato col path delle librerie

--- main_eigen.cpp
+++ main_eigen.cpp
@@ -1,11 +1,18 @@
-#include <eigen/Dense>
+#include <Eigen/Dense>
 #include <iostream>
 
 using namespace std;
+using Eigen::MatrixXd;
 
 int main( int argc, char** argv)
 {
-	cout << "Prova della libreria eigen" << endl;
 
+	cout << "Prova della libreria eigen" << endl;
+	Eigen::Matrix<int, 2, 2> m;
+	m(0,0) = 3;
+	m(1,0) = 2;
+	m(0,1) = -1;
+	m(1,1) = m(1,0) + m(0,1);
+	cout << "Er matricione: " << endl << m  << endl;
 	return 1;
 }
