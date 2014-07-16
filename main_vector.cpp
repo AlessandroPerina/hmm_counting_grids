9314f8cb4d90093b762913b6e6444a9fd50a96ee
Alessandro Perina <alessandro.perina@gmail.com>
2014-07-16 11:47:52 +0200

    counting grid class v1.0 - compila

---
+++ main_vector.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	vector<int> cg_size(3);
+	cg_size = {5,5,5};
+	vector<int> pos(3);
+	pos = {1,2,1};
+
+	int linear_index = pos[0];
+	int offset = cg_size[0];
+
+	for (int i = 1; i<pos.size(); i++)
+	{
+		linear_index += pos[i]*offset;
+		offset *= cg_size[i];
+	}
+
+	cout << "linear index = " << linear_index << endl;
+
+	return linear_index;
+}
